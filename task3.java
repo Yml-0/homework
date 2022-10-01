427d586f37b22a0da5eee1ce2e609d642db50db9
Yml0 <mailsart@mail.ru>
2022-10-01 19:49:33 +0300

    Loading homework2

---
+++ task3.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class task3 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int cakes = (n % 100 > 10 && n % 100 < 20  ? n % 100 : n % 10);
+        String result = "TORT";
+        switch (cakes) {
+                case 1:
+                    result += "";
+                    break;
+                case 2:
+                case 3:
+                case 4:
+                    result += "A";
+                    break;
+                default:
+                    result += "OV";
+
+        }
+        System.out.println(n + " " + result);
+
+    }
+}
